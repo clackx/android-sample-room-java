7ed7b8a7cf05f2e7a59dfa6e36a0d52d04495f19
clackx <clackxs@gmail.com>
2019-07-25 11:40:11 +0300

    Initial commit

---
+++ app/src/main/java/com/clackx/android_sample_room_java/MainActivity.java
@@ -0,0 +1,13 @@
+package com.clackx.android_sample_room_java;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
