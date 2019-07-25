eb3efa442d9feddb2ea264f8d2ec11264fc05248
clackx <clackxs@gmail.com>
2019-07-25 16:29:10 +0300

    Add a Room database

---
+++ app/src/main/java/com/clackx/android_sample_room_java/WordRoomDatabase.java
@@ -0,0 +1,28 @@
+package com.clackx.android_sample_room_java;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Word.class}, version = 1)
+public abstract class WordRoomDatabase extends RoomDatabase {
+    public abstract WordDao wordDao();
+
+    private static volatile WordRoomDatabase INSTANCE;
+
+    public WordRoomDatabase getDatabase (Context context) {
+        if (INSTANCE == null) {
+            synchronized (WordRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            WordRoomDatabase.class, "words_table")
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+}
