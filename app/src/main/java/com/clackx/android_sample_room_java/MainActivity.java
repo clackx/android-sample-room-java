63b1d93c71dad54ec3954672a3c0189aea7e00cd
clackx <clackxs@gmail.com>
2019-07-30 13:28:12 +0300

    Add XML layout and FAB support

--- app/src/main/java/com/clackx/android_sample_room_java/MainActivity.java
+++ app/src/main/java/com/clackx/android_sample_room_java/MainActivity.java
@@ -1,7 +1,12 @@
 package com.clackx.android_sample_room_java;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
+
 }
